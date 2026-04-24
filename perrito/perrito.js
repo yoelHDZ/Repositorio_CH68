4865cda9cf7628db2d1f7ff67cdc5b88b305c377
YoelHdz <yoel.hdz05@gmail.com>
2026-04-24 16:09:59 -0600

    Se corrigio el console.log donde mostraba la edad humana de perrito

--- perrito/perrito.js
+++ perrito/perrito.js
@@ -1,5 +1,4 @@
 let nombreP = prompt("Nombre del perrito:");
 let edadP = prompt("Que edad tiene el perrito:");
 
-console.log(nombreP+" tiene un nombre Genial!!! Sabias que tu perrito tiene "+edadp*7+" años humanos?");
-
+console.log(nombreP+" tiene un nombre Genial!!! Sabias que tu perrito tiene "+edadP*7+" años humanos?");
