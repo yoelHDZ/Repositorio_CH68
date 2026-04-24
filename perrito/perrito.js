a77c523b7118adf3b76dc413a1efe6bbb22fb2db
YoelHdz <yoel.hdz05@gmail.com>
2026-04-24 16:17:10 -0600

    Se añadio un ciclo if para definir la categoria de perrio, cachorro, adulto, anciano

--- perrito/perrito.js
+++ perrito/perrito.js
@@ -1,4 +1,10 @@
 let nombreP = prompt("Nombre del perrito:");
 let edadP = prompt("Que edad tiene el perrito:");
-
 console.log(nombreP+" tiene un nombre Genial!!! Sabias que tu perrito tiene "+edadP*7+" años humanos?");
+if (nombreP < 10) {
+    console.log("Tu perrito es un cachorro!!!");
+} else if (nombreP >= 10 && nombreP < 20) {
+    console.log("Tu perrito es un adulto!!!");
+} else {
+    console.log("Tu perrito es un anciano!!!");
+}
