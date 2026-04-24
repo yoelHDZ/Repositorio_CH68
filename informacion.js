cbf513d199efed8099df40d949312e1112e2da49
YoelHdz <yoel.hdz05@gmail.com>
2026-04-24 16:13:21 -0600

    Se añadio en informacion --> cosole.log la edad perruna del usuario

--- informacion.js
+++ informacion.js
@@ -1,4 +1,4 @@
 let nombre = prompt('¿Cuál es tu nombre?');
 let edad = prompt('¿Cuál es tu edad?');
-console.log(nombre) // undefined
+console.log("Sabias"+ nombre +" que tu tienes "+ edad/7+" años perrunos") // undefined
 
